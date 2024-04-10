1fd8e72b0a2fda85001af281ff3b0cec963678e5
Daniel <bakunzid@gmail.com>
2024-04-09 20:21:03 -0700

    Modified three files

--- script.js
+++ script.js
@@ -6,3 +6,23 @@ var typed = new Typed('.typing' ,
     loop:true
 
 })
+
+
+// circle skills
+
+
+const circles = document.querySelectorAll('.circle');
+circles.forEach(elem=>{
+
+    var dots = elem.getAttribute("data-dots");
+    var marked = elem.getAttribute("data-percent");
+    var percent = Math.floor(dots*marked/100);
+    var points = '';
+    var rotate = 360/dots;
+    for (let i = 0; i < dots ; i++) {
+
+        points += '<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>';
+    }
+     elem.innerHTML = points;
+
+})
