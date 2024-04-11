0fda09499be389010a9b73a83585b4a62d5c5dd6
Daniel <bakunzid@gmail.com>
2024-04-11 09:29:40 -0700

    modified two files

--- script.js
+++ script.js
@@ -12,17 +12,15 @@ var typed = new Typed('.typing' ,
 
 
 const circles = document.querySelectorAll('.circle');
-circles.forEach(elem=>{
-
+circles.forEach(elem =>{
     var dots = elem.getAttribute("data-dots");
     var marked = elem.getAttribute("data-percent");
     var percent = Math.floor(dots*marked/100);
-    var points = '';
-    var rotate = 360/dots;
-    for (let i = 0; i < dots ; i++) {
+    var points = "";
+    var rotate = 360 / dots;
 
-        points += '<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>';
+    for(let i = 0 ; i < dots ; i++){
+          points += `<div class="points" style="--i:${i}; --rot:${rotate}deg"></div>`;
     }
-     elem.innerHTML = points;
-
+       elem.innerHTML = points;
 })
